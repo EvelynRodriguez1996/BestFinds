f7106d16a46969715baad6b7da8ad74d6f720e34
AReaver <secre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.nameAReaversecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.emailzailancocmail.comsecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global --unset user.namesecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global --unset user.emailsecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.name AReaversecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.email>
2023-11-07 15:06:59 -0600

    Se agrega repositorio de proyecto final

---
+++ assets/js/contacto.js
@@ -0,0 +1,78 @@
+let nomUsuario = document.getElementById("nombre");
+let emailUsuario = document.getElementById("correo");
+let telUsuario = document.getElementById("telefono");
+let mensaje = document.getElementById("txtMensaje");
+let btnEnviar = document.getElementById("colorB");
+
+function enviarFormulario(){
+    let nomUsuarioValue = nomUsuario.value;
+    let emailUsuarioValue = emailUsuario.value;
+    let telUsuarioValue = telUsuario.value;
+    let mensajeValue = mensaje.value;
+
+    if(nomUsuarioValue == "" || emailUsuarioValue == "" || telUsuarioValue == "" || mensajeValue == ""){
+        alert("Todos los campos son obligatorios");
+    }else{
+        alert("Mensaje enviado correctamente");
+    }
+}
+
+let formulario = document.getElementById('formFormulario');
+let inputs = document.querySelectorAll('#formFormulario input');
+
+//Expresiones permitidas para cada campo
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+let campos = {
+	nombre: false,
+	correo: false,
+    telefono: false
+}
+
+//Switch para validar cada campo
+let validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'Nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'Correo');
+		break;
+        case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'Telefono');
+		break;
+	}
+}
+
+//En caso de que se den expresiones invalidas, se muestra la alerta
+let validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		
+		document.getElementById(`alert${campo}`).classList.remove('alert-activa');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`alert${campo}`).classList.add('alert-activa');
+		campos[campo] = false;
+	}
+}
+
+//Se comprueba cada que se realicen los siguientes eventos
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);//Cada que presiona una teclas
+	input.addEventListener('blur', validarFormulario);//Cada que se de click fuera del input
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
+
+
+
+
+
+
+
+btnEnviar.addEventListener("click", enviarFormulario)
---
+++ assets/js/productos2.js
@@ -0,0 +1,113 @@
+////////////////////AGREGAR COMENTARIO/////////////////////////
+// Se obtiene el elemento del botón de agregar comentario mediante su ID
+const botonComentario = document.getElementById('btnEnviarComentario');
+let comentarios = document.getElementById('txtComentario');
+let seccionComentarios = document.getElementById('seccionDescYComentarios');
+
+function agregarComentario() {
+    let comentario = comentarios.value;
+    let nuevoComentario = document.createElement('div');
+    nuevoComentario.innerHTML = `<div id="contenedorInferior">
+    <h5>Usuario Anónimo</h5>
+    <p id="comentario">
+        ${comentario}
+    </p>
+    <div id="der">
+        <button id="boton-corazon" class="boton-corazon">
+            <span id="contador-likes">0</span>
+            <i class="fa fa-heart"></i>
+        </button>
+    </div>
+</div>`;
+    seccionComentarios.appendChild(nuevoComentario);
+    comentarios.value = "";
+}
+
+botonComentario.addEventListener("click", agregarComentario);
+
+
+
+
+
+
+
+// Función que contabiliza los likes para un comentario específico
+function contabilizarLikes(comentarioId) {
+    const botonCorazon = document.getElementById(`boton-corazon-${comentarioId}`);
+    const contadorLikes = document.getElementById(`contador-likes-${comentarioId}`);
+
+    let likes = 0;
+    let Liked = false;
+
+    function actualizarLikes() {
+        if (Liked) {
+            likes--;
+        } else {
+            likes++;
+        }
+
+        Liked = !Liked;
+        contadorLikes.innerText = likes;
+        botonCorazon.classList.toggle('activo');
+    }
+
+    botonCorazon.addEventListener('click', actualizarLikes);
+}
+
+// Llamar a la función para cada comentario en un bucle)
+contabilizarLikes(1); // Para el primer comentario
+contabilizarLikes(2); // Para el segundo comentario, etc.
+
+
+
+
+/////////////////////////////Collapse de comentarios//////////////////////////////
+let comentariosDiv = document.getElementById('comentariosDiv');
+comentariosDiv.innerHTML=` <div id="contenedorDeComentarios">
+<h4>Comentarios</h4>
+<div id="contenedorAgregarComentario">
+    <h5>Agrega un comentario</h5>
+    <form>
+        <textarea class="form-control" id="txtComentario" rows="2"
+            placeholder="Tu opinion nos ayuda mucho :)..."></textarea>
+        <div id="der">
+            <button type="button" class="btn btn-primary"
+                id="btnEnviarComentario">Enviar</button>
+        </div>
+    </form>
+</div>
+
+<div id="contenedorInferior">
+    <h5>Usuario Anónimo</h5>
+    <p id="comentario">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, praesentium
+        expedita
+        dolore eligendi cumque commodi harum impedit facere mollitia ad quibusdam deleniti
+        cupiditate reiciendis ullam dolorum, eius, magni aliquam! Similique.
+    </p>
+    <div id="der">
+        <button id="boton-corazon-1" class="boton-corazon">
+            <!-- Utilizamos "fa" para FontAwesome -->
+            <span id="contador-likes-1">0</span>
+            <i class="fa fa-heart"></i>
+        </button>
+    </div>
+</div>
+
+<div id="contenedorInferior">
+    <h5>Usuario Anónimo</h5>
+    <p id="comentario">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, praesentium
+        expedita
+        dolore eligendi cumque commodi harum impedit facere mollitia ad quibusdam deleniti
+        cupiditate reiciendis ullam dolorum, eius, magni aliquam! Similique.
+    </p>
+    <div id="der">
+        <button id="boton-corazon-2" class="boton-corazon">
+            <!-- Utilizamos "fa" para FontAwesome -->
+            <span id="contador-likes-2">0</span>
+            <i class="fa fa-heart"></i>
+        </button>
+    </div>
+</div>
+</div>`;
